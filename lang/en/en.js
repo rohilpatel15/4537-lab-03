2c37fc9d5ba299c4a47d7194eb0b7cd1423b3d23
rohilpatel15 <rohilpatel123@outlook.com>
2024-02-01 15:41:46 -0800

    added user message to en.js

--- lang/en/en.js
+++ lang/en/en.js
@@ -1,8 +1,15 @@
 const messages = {
     greeting: "Hello",
-    message: ", What a beautiful day. Server current date and time is "
+    message: ", What a beautiful day. Server current date and time is ",
+    landPage: "Add your name after the = then put it into the search bar. ",
+    link: "For Example: https://squid-app-c8omt.ondigitalocean.app/COMP4537/labs/3/getDate?name=Rohil"
 };
 
-module.exports = (name) => {
-    return `${messages.greeting} ${name}${messages.message}`;
-};
+module.exports = {
+    greetWithName: (name) => {
+      return `${messages.greeting} ${name}${messages.message}`;
+    },
+    greetOnLoad: () =>{
+      return `${messages.landPage} ${messages.link}`;
+    }
+  };
--- server.js
+++ server.js
@@ -6,17 +6,18 @@ const greetMessage = require('./lang/en/en');
 const server = http.createServer((req, res) => {
   const parsedUrl = url.parse(req.url, true);
   const { pathname, query } = parsedUrl;
+  const landPage = greetMessage.greetOnLoad();
 
   if (pathname === '/COMP4537/labs/3/getDate' && query.name) {
     const name = query.name;
     const currentTime = date.getDate();
-    const response = `<h1 style = "color: blue;">${greetMessage(name)} ${currentTime}</h1>`;
+    const response = `<h1 style = "color: blue;">${greetMessage.greetWithName(name)} ${currentTime}</h1>`;
 
       res.writeHead(200, { 'Content-Type': 'text/html' });
       res.end(response);
     } else {
       res.writeHead(404, { 'Content-Type': 'text/html' });
-      res.end('Add your name after the = then put it into the search bar. For Example: https://squid-app-c8omt.ondigitalocean.app/COMP4537/labs/3/getDate?name=Rohil');
+      res.end(landPage);
     }
 
   }).listen(process.env.PORT || 3000);
